fe4629949bc8021680d3572b1d6b16ffb17945c2
nathanremond <perso.nathan.remond@gmail.com>
2025-01-09 17:03:07 +0100

    [feat] Ajout header + footer

--- src/Views/includes/footer.php
+++ src/Views/includes/footer.php
@@ -1,3 +1,13 @@
-<?php
-
-//TODO
+<footer class="footer">
+    <div class="social-links">
+        <a href="#" class="social-icon">
+            <img src="/images/Logo_facebook.png" alt="Facebook">
+        </a>
+        <a href="#" class="social-icon">
+            <img src="/images/Logo_twitter.png" alt="Twitter">
+        </a>
+        <a href="#" class="social-icon">
+            <img src="/images/Logo_insta.png" alt="Instagram">
+        </a>
+    </div>
+</footer>
--- src/Views/includes/header.php
+++ src/Views/includes/header.php
@@ -1,3 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alfredo's Park</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+</head>
+<body>
 
-//TODO
+<header>
+    <div class="header-container">
+        <!-- Logo -->
+        <div class="logo">
+            <img src="/images/Logo_Alfredo_Prk.png" alt="Logo du site" width="100">
+        </div>
+
+        <!-- Menu -->
+        <nav class="menu">
+            <a href="#">Accueil</a>
+            <a href="#">Billetterie</a>
+            <a href="#">Attraction et Resto</a>
+        </nav>
+
+        <!-- Icone à droite -->
+        <div class="icon">
+            <img src="/images/utilisateurAlfredo.png" alt="Icône cliquable">
+        </div>
+</header>
+        
+    
+
+</body>
+</html>
