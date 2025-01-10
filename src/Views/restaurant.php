d8dc107cd4a13d4f68af263e1347e3eb539e7745
Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>
2025-01-10 12:20:00 +0100

    [feat]Ajout d'un formulaire dans la page restaurant

--- src/Views/restaurant.php
+++ src/Views/restaurant.php
@@ -8,3 +8,12 @@
         <img src="<?=$menu['url_picture']?>" alt="image <?=$menu['name']?>"></img>
         <p><?=$menu['price']?></p>
 <?php } ?>
+
+<h2>Reservation</h2>
+<form action="/" method="post">
+    <label for="book_ticket">Nombre de places :</label>
+    <input type="number" id="book_ticket" name="book_ticket" min="0">
+    <label for="visit_date">Date et heure de réservation :</label>
+    <input type="datetime-local" id="visit_date" name="visit_date" required>
+    <button type="submit">Reserver</button>
+</form>
